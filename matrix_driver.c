8b10d2e954a819f0d1f0da3e74d773f9ff34e10b
aleksabu0 <aleksabu0@gmail.com>
2022-02-16 18:49:13 +0100

    v2

--- matrix_driver.c
+++ matrix_driver.c
@@ -23,7 +23,7 @@
 
 
 
- MODULE_LICENSE("GPL");
+MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Driver for matrix ouvput");
 #define DEVICE_NAME "matrix"
 #define DRIVER_NAME "matrix_driver"
@@ -40,6 +40,7 @@ static ssize_t matrix_write(struct file *f, const char __user *buf, size_t lengt
 static int __init matrix_init(void);
 static void __exit matrix_exit(void);
 static int matrix_remove(struct platform_device *pdev);
+void extract_matrix(char store_mat[50], int mat[50],int dim[]);
 
 //*********************GLOBAL VARIABLES*************************************
 struct matrix_info {
@@ -53,6 +54,11 @@ static struct class *my_class;
 static struct device *my_device;
 static int int_cnt;
 static struct matrix_info *vp = NULL;
+
+int dimA[] = {0, 0};
+int dimB[] = {0, 0};
+int matA[50], matB[50];
+char store_matA[50], store_matB[50];
 
 static struct file_operations my_fops =
   {
@@ -102,13 +108,13 @@ static int matrix_probe(struct platform_device *pdev)
   // Get memory for structure matrix_info
   vp = (struct matrix_info *) kmalloc(sizeof(struct matrix_info), GFP_KERNEL);
   if (!vp) {
-    printk(KERN_ALERT "matrix_probe: Could not allocate timer device\n");
+    printk(KERN_ALERT "matrix_probe: Could not allocate memory\n");
     return -ENOMEM;
   }
   // Put phisical adresses in timer_info structure
   vp->mem_start = r_mem->start;
   vp->mem_end = r_mem->end;
-    
+  printk(KERN_INFO "matrix_probe: MEMORY LOCATION",(void *)vp->mem_start);  
   // Reserve that memory space for this driver
   if (!request_mem_region(vp->mem_start,vp->mem_end - vp->mem_start + 1, DRIVER_NAME))
   {
@@ -138,14 +144,14 @@ static int matrix_remove(struct platform_device *pdev)
 {
   int i = 0;
   // Exit Device Module
-  for (i = 0; i < (256*144); i++) 
+  for (i = 0; i <= (32*49); i++) 
   { 
     iowrite32(i*4, vp->base_addr + 8); 
     iowrite32(0, vp->base_addr); 
   } 
   printk(KERN_INFO "matrix_remove: matrix remove in process");
   iounmap(vp->base_addr);
-  release_mem_region(vp->mem_start, vp->mem_end - vp->mem_start + 1);
+  release_mem_region(vp->mem_start, (vp->mem_end - vp->mem_start + 1));
   kfree(vp);
   printk(KERN_INFO "matrix_remove: matrix driver removed");
   return 0;
@@ -156,64 +162,133 @@ static int matrix_remove(struct platform_device *pdev)
 
 static int matrix_open(struct inode *i, struct file *f)
 {
-  //printk(KERN_INFO "matrix opened\n");
+  printk(KERN_INFO "matrix opened\n");
   return 0;
 }
 static int matrix_close(struct inode *i, struct file *f)
 {
-  //printk(KERN_INFO "matrix closed\n");
+  printk(KERN_INFO "matrix closed\n");
   return 0;
 }
 static ssize_t matrix_read(struct file *f, char __user *buf, size_t len, loff_t *off)
 {
-  //printk("matrix read\n");
+  printk(KERN_INFO,"matrix read\n");
   return 0;
 }
 static ssize_t matrix_write(struct file *f, const char __user *buf, size_t length, loff_t *off)
 {	
   char buff[BUFF_SIZE];
   int ret = 0;
-  unsigned int xpos=0,ypos=0;
-  unsigned long long rgb=0;
-  unsigned char rgb_buff[10];  
   ret = copy_from_user(buff, buf, length);  
   if(ret){
     printk("copy from user failed \n");
     return -EFAULT;
   }  
-  buff[length] = '\0';
+    buff[length] = '\0';
   
-  
-  sscanf(buff,"%d,%d,%s", &xpos, &ypos, rgb_buff);  
-  ret = kstrtoull(rgb_buff, 0, &rgb);
+    sscanf(buffer, "%[^*]*%s" , store_matA, store_matB);
+
+    extract_matrix(store_matA, matA, dimA);
+    extract_matrix(store_matB, matB, dimB);
+	
+    if(dimA[1] != dimB[0]){
+        //printf("\nError! DiffDim\n");
+        return -1;
+    }
+
+    //printf("\ndimA: %dx%d", dimA[0],dimA[1]);
+    //printf("\ndimB: %dx%d", dimB[0],dimB[1]);
  
-  if(ret != -EINVAL)//checking for parsing error
-  {
-    if (xpos > 255)
-    {
-      printk(KERN_WARNING "matrix_write: X_axis position exceeded, maximum is 255 and minimum 0 \n");
-    }
-    else if (ypos > 143)
-    {
-      printk(KERN_WARNING "matrix_write: Y_axis position exceeded, maximum is 143 and minimum 0 \n");
-    }
-    else
-    {
-      iowrite32((256*ypos + xpos)*4, vp->base_addr + 8);
-      iowrite32(rgb, vp->base_addr);      
-    }
-  }
-  else
+  //if(ret != -EINVAL)//checking for parsing error
+  //{
+    iowrite32((256*ypos + xpos)*4, vp->base_addr + 8);
+    iowrite32(rgb, vp->base_addr);         
+  //}
+  /*else
   {
     printk(KERN_WARNING "matrix_write: Wrong write format, expected \"xpos,ypos,rgb\"\n");
     // return -EINVAL;//parsing error
-  }        
+  }  */      
   return length;
-
 }
 
 //***************************************************
-// HELPER FUNCTIONS (STRING TO INTEGER)
+// HELPER FUNCTIONS (READ MATRIX)
+
+void extract_matrix(char store_mat[50], int mat[50],int dim[])
+{
+    int i, j=0, k=0;
+    int n=0, m=0;
+    char num[5];
+    int numlen=0;
+
+    for (i = 0; store_mat[i] != '\0'; i++);
+    int len = i;
+
+    for (i = 1; i<len-1; i++)
+    {
+        if(store_mat[i]==']')
+        {
+            num[numlen]='\0';
+            mat[j]=atoi(num);
+            j++;
+
+            for(int i=0; i<5; i++)
+            {
+                num[i]=0;
+            }
+            numlen=0;
+            m++;
+            if(!k)
+            {
+                dim[1] = m;
+                k++;
+            }
+            if(m != dim[1])
+            {
+                printf("\nError!\n");
+                return -1;
+            }
+            m=0;
+        }
+
+        if(store_mat[i]=='[')
+        {
+            n++;
+        }
+
+        if(store_mat[i]==',' && store_mat[i+1]!='[')
+        {
+            num[numlen]='\0';
+            mat[j]=atoi(num);
+            j++;
+            for(int i=0; i<5; i++)
+            {
+                num[i]=0;
+            }
+            numlen=0;
+            m++;
+
+        }
+
+        if(store_mat[i] >= 48 && store_mat[i] <= 57)
+        {
+            num[numlen]=store_mat[i];
+            numlen++;
+        }
+    }
+    dim[0]=n;
+    if(dim[0] > 7 || dim[1] > 7){
+        //printf("\nMaxDim : 7x7");
+        return -1;
+    }
+    for(int i=0; i<dim[0]*dim[1];i++){
+         if(mat[i] > 4096){
+            //printf("\nMaxNum : 4096");
+            return -1;
+         }
+    }
+}
 
 
 //***************************************************
@@ -240,7 +315,7 @@ static int __init matrix_init(void)
     goto fail_0;
   }
   printk(KERN_INFO "Succ class chardev1 create!.\n");
-  my_device = device_create(my_class, NULL, MKDEV(MAJOR(my_dev_id),0), NULL, "matrix");
+  my_device = device_create(my_class, NULL, MKDEV(MAJOR(my_dev_id),0), NULL, "matmul");
   if (my_device == NULL)
   {
     goto fail_1;
@@ -289,3 +364,5 @@ MODULE_AUTHOR ("FTN");
 MODULE_DESCRIPTION("Test Driver for matrix output.");
 MODULE_LICENSE("GPL v2");
 MODULE_ALIAS("custom:matrix");
+
+
